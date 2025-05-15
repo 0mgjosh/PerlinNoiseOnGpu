0c0e319e6ace0830807ad0a562edd135681deb5f
joshu <joshu@DAMNTHING>
2025-05-15 00:40:10 -0700

    Add project files.

---
+++ Project1/Content/effect.fx
@@ -0,0 +1,95 @@
+﻿#if OPENGL
+	#define SV_POSITION POSITION
+	#define VS_SHADERMODEL vs_3_0
+	#define PS_SHADERMODEL ps_3_0
+#else
+	#define VS_SHADERMODEL vs_4_0_level_9_1
+	#define PS_SHADERMODEL ps_4_0_level_9_1
+#endif
+
+Texture2D SpriteTexture;
+float spin;
+float scale;
+
+sampler2D SpriteTextureSampler = sampler_state
+{
+	Texture = <SpriteTexture>;
+};
+
+struct VertexShaderOutput
+{
+	float4 Position : SV_POSITION;
+	float4 Color : COLOR0;
+	float2 TextureCoordinates : TEXCOORD0;
+};
+
+float2 randomGradient(float2 p)
+{
+    p = p + 0.02;
+    float x = dot(p, float2(123.4, 234.5));
+    float y = dot(p, float2(234.5, 345.6));
+    float2 gradient = float2(x, y);
+    gradient = sin(gradient);
+    gradient = gradient * 43758.5453;
+    gradient = sin(gradient + spin);
+    return gradient;
+}
+
+float2 quintic(float2 p)
+{
+    return p * p * p * (10.0 + p * (-15.0 + p * 6.0));
+}
+
+float4 MainPS(VertexShaderOutput input) : COLOR
+{
+    float3 white = float3(1,1,1);
+    float3 black = float3(0,0,0);
+    float3 col = black;
+
+    float2 uv = input.TextureCoordinates;
+	
+    uv *= scale;
+	
+    float2 gridID = floor(uv);
+    float2 gridUV = frac(uv);
+    col = float3(gridID, 0);
+    col = float3(gridUV, 0);
+	
+    float2 bl = gridID;
+    float2 br = gridID + float2(1, 0);
+    float2 tl = gridID + float2(0, 1);
+    float2 tr = gridID + float2(1, 1);
+    
+    float2 gradBl = randomGradient(bl);
+    float2 gradBr = randomGradient(br);
+    float2 gradTl = randomGradient(tl);
+    float2 gradTr = randomGradient(tr);
+    
+    float2 distFromPixelToBl = gridUV;
+    float2 distFromPixelToBr = gridUV - float2(1, 0);
+    float2 distFromPixelToTl = gridUV - float2(0, 1);
+    float2 distFromPixelToTr = gridUV - float2(1, 1);
+    
+    float dotBl = dot(gradBl, distFromPixelToBl);
+    float dotBr = dot(gradBr, distFromPixelToBr);
+    float dotTl = dot(gradTl, distFromPixelToTl);
+    float dotTr = dot(gradTr, distFromPixelToTr);
+    
+    gridUV = quintic(gridUV);
+
+    float b = lerp(dotBl, dotBr, gridUV.x);
+    float t = lerp(dotTl, dotTr, gridUV.x);
+    float perlin = lerp(b, t, gridUV.y);
+	
+    perlin += 0.1;
+        
+    return float4(perlin,perlin,perlin,1);
+}
+
+technique SpriteDrawing
+{
+	pass P0
+	{
+		PixelShader = compile PS_SHADERMODEL MainPS();
+	}
+};
---
+++ Project1/Game1.cs
@@ -0,0 +1,96 @@
+﻿using Microsoft.Xna.Framework;
+using Microsoft.Xna.Framework.Graphics;
+using Microsoft.Xna.Framework.Input;
+
+namespace Project1
+{
+    public class Game1 : Game
+    {
+        private GraphicsDeviceManager _graphics;
+        private SpriteBatch _spriteBatch;
+
+        private Vector2 _screen = new(1280, 1280);
+        private SpriteFont _font;
+        private Effect _effect;
+        private Texture2D _pixel;
+        private RenderTarget2D _renderTarget;
+
+        private float spin = 0;
+        private float scale = 1;
+
+        public Game1()
+        {
+            _graphics = new GraphicsDeviceManager(this);
+            Content.RootDirectory = "Content";
+            IsMouseVisible = true;
+        }
+
+        protected override void Initialize()
+        {
+            _graphics.PreferredBackBufferWidth = (int)_screen.X;
+            _graphics.PreferredBackBufferHeight = (int)_screen.Y;
+            _graphics.ApplyChanges();
+
+            base.Initialize();
+        }
+
+        protected override void LoadContent()
+        {
+            _spriteBatch = new SpriteBatch(GraphicsDevice);
+            _font = Content.Load<SpriteFont>("font");
+            _effect = Content.Load<Effect>("effect");
+            _pixel = new Texture2D(GraphicsDevice, 1, 1);
+            _pixel.SetData([Color.White]);
+            _renderTarget = new RenderTarget2D(GraphicsDevice, (int)_screen.X, (int)_screen.Y);
+        }
+
+        float previous_Scroll_Value;
+        protected override void Update(GameTime gameTime)
+        {
+            if (GamePad.GetState(PlayerIndex.One).Buttons.Back == ButtonState.Pressed || Keyboard.GetState().IsKeyDown(Keys.Escape))
+                Exit();
+            KeyboardState kstate = Keyboard.GetState();
+            MouseState mstate = Mouse.GetState();
+
+            if(kstate.IsKeyDown(Keys.Left)) spin -= 0.1f;
+            if(kstate.IsKeyDown(Keys.Right)) spin += 0.1f;
+            if (mstate.ScrollWheelValue > previous_Scroll_Value)
+            {
+                scale += 0.5f;
+                previous_Scroll_Value = mstate.ScrollWheelValue;
+            }
+            if (mstate.ScrollWheelValue < previous_Scroll_Value)
+            {
+                scale -= 0.5f;
+                previous_Scroll_Value = mstate.ScrollWheelValue;
+            }
+
+                _effect.Parameters["spin"].SetValue(spin);
+            _effect.Parameters["scale"].SetValue(scale);
+
+            base.Update(gameTime);
+        }
+
+        protected override void Draw(GameTime gameTime)
+        {
+            GraphicsDevice.Clear(Color.Black);
+
+            // Draw To Target
+            GraphicsDevice.SetRenderTarget(_renderTarget);
+            _spriteBatch.Begin(samplerState: SamplerState.PointClamp, sortMode: SpriteSortMode.Immediate);
+            _effect.CurrentTechnique.Passes[0].Apply();
+            _spriteBatch.Draw(_pixel, new Rectangle(0, 0, (int)_screen.X, (int)_screen.Y), Color.White);
+
+            _spriteBatch.End();
+            GraphicsDevice.SetRenderTarget(null);
+
+            // Final Draw
+            _spriteBatch.Begin(samplerState: SamplerState.PointClamp, sortMode: SpriteSortMode.Immediate);
+
+            _spriteBatch.Draw(_renderTarget, new Rectangle(0, 0, (int)_screen.X, (int)_screen.Y), Color.White);
+
+            _spriteBatch.End();
+            base.Draw(gameTime);
+        }
+    }
+}
